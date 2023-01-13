40c3dd5694ad7e445b78208ed6267643b153d910
gutembergg <gmascarenhas3001@gmail.com>
2023-01-13 16:21:29 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <div>
+        <a target="_blank" href="https://vitejs.dev">
+            <img alt="Vite logo" src="/vite.svg" class="logo" />
+        </a>
+        <img
+            :key="i"
+            v-for="i in items"
+            alt="Vue logo"
+            src="./assets/logo.png"
+        />
+        <a href="https://vuejs.org/" target="_blank">
+            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+</template>
+
+<script>
+var test = 'test'
+</script>
+
+<style scoped>
+.logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+}
+.logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
